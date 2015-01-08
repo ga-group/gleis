d97cb85911fae3f8f58d231b6c3853649e9a9bb6
Sebastian Freundt <hroptatyr@fresse.org>
2015-01-08 23:48:27 +0000

    import queries for gnd/viaf references

---
+++ sql/company_keypeople_gnd.sql
@@ -0,0 +1,26 @@
+SPARQL
+DEFINE input:inference <http://dbpedia.org/ontology/>
+DEFINE input:same-as "yes"
+PREFIX lei: <http://www.leiroc.org/data/schema/leidata/2014/>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+CONSTRUCT {?company ?p ?person} WHERE {
+	GRAPH <http://datahub.io/dataset/legal-entities> {?company a lei:LEI .}
+	{
+		{
+			BIND (<http://dbpedia.org/ontology/keyPerson> as ?p)
+			GRAPH <http://company.dbpedia.org/> {?company ?p ?res .}
+		} UNION {
+			BIND (<http://dbpedia.org/ontology/chairman> as ?p)
+			GRAPH <http://company.dbpedia.org/> {?company ?p ?res .}
+		} UNION {
+			BIND (<http://dbpedia.org/property/founder> as ?p)
+			GRAPH <http://company.dbpedia.org/> {?company ?p ?res .}
+		}
+		{
+			?res <http://dbpedia.org/property/gnd> ?x .
+		} UNION {
+			?res <http://de.dbpedia.org/property/gnd> ?x .
+		}
+		BIND(IRI(CONCAT('http://d-nb.info/gnd/', ?x)) AS ?person)
+	}
+};
